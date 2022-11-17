06debcf30ebd2f55dd437e068d94be2161d69b46
Ajai Kannan <aj.kannan1101@gmail.com>
2022-11-17 21:10:55 +0530

    Late commit.

--- components/Stories/StoryCard.js
+++ components/Stories/StoryCard.js
@@ -1,13 +1,11 @@
 import Link from "next/link";
 import { linkResolver } from "../../prismic-configuration";
 
-const StoryCard = ({
-  story
-}) => {
+const StoryCard = ({ story }) => {
   return (
     <div className="flex flex-row md:px-10 items-center justify-center mb-7 md:mb-10">
       <div className="basis-2/5 md:basis-1/4">
-        <h3 className="text-orange text-xs md:text-base uppercase content-bold mb-1">
+        <h3 className="text-[#37b113] text-xs md:text-base uppercase content-bold mb-1">
           {story.data.category.data.name}
         </h3>
         <Link href={linkResolver(story)} passHref>
@@ -34,18 +32,18 @@ const StoryCard = ({
           <Link href={linkResolver(story.data.author)} passHref>
             <a>
               <img
-                className="w-[24px] md:w-[36px] h-auto mr-1 p-[1px] rounded-full border-2 border-darkgreen"
+                className="w-[24px] md:w-[32px] h-auto mr-2 p-[1px] rounded-full border-2 border-darkgreen"
                 src={story.data.author.data.picture.url}
               />
             </a>
           </Link>
           <Link href={linkResolver(story.data.author)} passHref>
-            <a className="self-center text-xs md:text-base mr-3 md:mr-5">
+            <a className="self-center text-xs md:text-sm mr-3 md:mr-5">
               <p>{story.data.author.data.name}</p>
             </a>
           </Link>
 
-          <p className="self-center text-xs md:text-base text-grey-600">
+          <p className="self-center text-xs md:text-xs text-grey-600">
             {story.data.date}
           </p>
         </div>
